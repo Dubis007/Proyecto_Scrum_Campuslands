9d55dbcdb9d04a1eb90174614b70fd0d06fa8125
JuanDavid <davidesparzaj22@gmail.com>
2026-08-27 21:51:55 -0500

    refactor: Renombrar funcion de registro de estudiantes

--- 09_Codigo/main.py
+++ 09_Codigo/main.py
@@ -1,5 +1,5 @@
 from equipos import consultar_equipos
-from estudiantes import pedir_registro, mostrar_estudiantes
+from estudiantes import pedir_registro_estudiante, mostrar_estudiantes
 
 def menu_principal():
     while True:
@@ -17,7 +17,7 @@ def menu_principal():
         elif opcion == "2":
                 consultar_equipos(solo_disponibles=True)
         elif opcion == "3":
-                pedir_registro()
+                pedir_registro_estudiante()
         elif opcion == "4":
                 mostrar_estudiantes()
         elif opcion == "0":
